12c456bf21ff2f25237a552629fb5a2ee26b5d7d
kondykov <kondykovs@gmail.com>
2023-10-04 14:00:57 +0300

    Indev 1.2.2b

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use Illuminate\Support\Facades\Route;
 
 route::middleware('guest')->group(function(){
   route::get('/', [dashboardController::class, 'index'])->name('dashboard');
-  route::get('/orderlist', [orderController::class, 'index'])->name('order.index');
-  route::get('/orderlist/create', [orderController::class, 'create'])->name('order.create');
-  route::get('/orderlist/store', [orderController::class, 'store'])->name('order.store');
-  route::get('/orderlist/{order}', [orderController::class, 'show'])->name('order.show');
-  route::get('/orderlist/{order}/edit', [orderController::class, 'edit'])->name('order.edit');
-  route::get('/orderlist/{order}/sub', [orderController::class, 'put'])->name('order.put');
-  route::get('/orderlist/{order}/commit', [orderController::class, 'commit'])->name('order.commit');
-  
-  route::get('/orderlist/{order}/completing', [orderController::class, 'complete'])->name('order.complete');
-
+  route::prefix('/orderlist')->group(function(){
+    route::get('/', [orderController::class, 'index'])->name('order.index');
+    route::get('/create', [orderController::class, 'create'])->name('order.create');
+    route::get('/store', [orderController::class, 'store'])->name('order.store');
+    route::get('/{order}', [orderController::class, 'show'])->name('order.show');
+    route::get('/{order}/edit', [orderController::class, 'edit'])->name('order.edit');
+    route::get('/{order}/sub', [orderController::class, 'put'])->name('order.put');
+    route::get('/{order}/commit', [orderController::class, 'commit'])->name('order.commit');
+    route::get('/{order}/completing', [orderController::class, 'complete'])->name('order.complete');
+  });
 });
 // middleware — auth
 route::middleware('guest')->group(function(){
